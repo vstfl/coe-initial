abdd182f58f0e0c7cea688200cca6e1c41c2fe05
vstfl <vastfulmusic@gmail.com>
2025-10-24 09:37:59 -0600

    Prepare Github Pages build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')?.[1] ?? 'coe-initial';
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
+  base: isProduction ? `/${repoName}/` : '/',
   plugins: [svelte()],
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  }
 });
